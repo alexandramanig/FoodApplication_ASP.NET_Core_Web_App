84f63e2bcf9592697d2e4fd8051ed28dd234ce83
Mănig Alexandra-Iulia <alexandramanig_iulia@yahoo.com>
2025-01-12 12:58:02 +0200

    Recipes

--- FoodApplication/wwwroot/js/site.js
+++ FoodApplication/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let apiURL = "https://forkify-api.herokuapp.com/api/v2/recipes";
+let apikey = "7ecac8c2-fcdf-42cf-bfca-0d9f6d9cf4b1";
+
+async function GetRecipes(recipeName,id,isAllShow) {
+    let resp = await fetch(`${apiURL}?search=${recipeName}&key=${apikey}`);
+    let result = await resp.json();
+    let Recipes = isAllShow ? result.data.recipes : result.data.recipes.slice(1, 7);
+    showRecipe(Recipes,id);
+}
+function showRecipe(recipes, id) {
+    $.ajax({
+        contentType: "application/json; charset=utf-8",
+        dataType: 'html',
+        type: 'POST',
+        url: '/Recipe/GetRecipeCard',
+        data: JSON.stringify(recipes),
+        success: function (htmlResult) {
+            $('#' + id).html(htmlResult);
+        },
+
+    });
+}
