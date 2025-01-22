235aa87105b6b0ee88761ea8c54e844f55c6c026
Mănig Alexandra-Iulia <alexandramanig_iulia@yahoo.com>
2025-01-22 16:30:20 +0200

    Login Functionality

--- FoodApplication/Controllers/AccountController.cs
+++ FoodApplication/Controllers/AccountController.cs
@@ -19,6 +19,18 @@ namespace FoodApplication.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public async Task <IActionResult> Login (LoginViewModel login)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(login.Email, login.Password, false, false);
+                if (result.Succeeded) 
+                    return RedirectToAction("Index", "Home");
+                ModelState.AddModelError("", "Invalid LogIn Attempt");
+            }
+            return View(login);
+        }
 
         public IActionResult Register() 
         {
