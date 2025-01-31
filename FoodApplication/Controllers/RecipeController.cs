58066270e15ac7ba9bc9d1dce087b4007272d93b
Mănig Alexandra-Iulia <alexandramanig_iulia@yahoo.com>
2025-01-31 16:17:06 +0200

    Save order

--- FoodApplication/Controllers/RecipeController.cs
+++ FoodApplication/Controllers/RecipeController.cs
@@ -2,15 +2,19 @@
 using FoodApplication.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
+using FoodApplication.ContectDBConfig;
 
 namespace FoodApplication.Controllers
 {
     public class RecipeController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        public RecipeController(UserManager<ApplicationUser> userManager)
+        private readonly FoodApplicationDBContext context;
+        public RecipeController(UserManager<ApplicationUser> userManager,
+            FoodApplicationDBContext dBContext)
         {
             _userManager = userManager;
+            context = dBContext;
         }
 
         public IActionResult Index()
@@ -45,9 +49,16 @@ namespace FoodApplication.Controllers
         }
         [HttpPost]
         [Authorize]
-        public IActionResult Order(Order order)
+        public IActionResult Order([FromForm]Order order)
         {
-            return View(order);
+            order.OrderDate = DateTime.Now;
+            if (ModelState.IsValid)
+            {
+                context.Orders.Add(order);
+                context.SaveChanges();
+                return RedirectToAction("Index","Recipe");
+            }
+            return RedirectToAction("Order","Recipe", new {id=order.Id});
         }
     }
 }
