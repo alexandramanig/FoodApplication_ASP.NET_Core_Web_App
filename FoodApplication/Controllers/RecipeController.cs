ae7e8acadc482d84ecad8df12f7ff0c93e7cf382
Mănig Alexandra-Iulia <alexandramanig_iulia@yahoo.com>
2025-01-31 14:12:03 +0200

    Show User Details

--- FoodApplication/Controllers/RecipeController.cs
+++ FoodApplication/Controllers/RecipeController.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using FoodApplication.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace FoodApplication.Controllers
 {
     public class RecipeController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public RecipeController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -19,17 +26,20 @@ namespace FoodApplication.Controllers
             ViewBag.Recipe = recipe;
             return View();
         }
-        public IActionResult Order([FromQuery]string id)
+        public IActionResult Order([FromQuery] string id)
         {
             ViewBag.Id = id;
             return View();
         }
 
         [HttpPost]
-        public IActionResult ShowOrder(OrderRecipeDetails orderRecipeDetails)
+        public async Task<IActionResult> ShowOrder(OrderRecipeDetails orderRecipeDetails)
         {
             Random random = new Random();
             ViewBag.Price = Math.Round(random.Next(150,500)/5.0)*5;
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            ViewBag.UserId = user?.Id;
+            ViewBag.Address = user?.Address;
             return PartialView("_ShowOrder", orderRecipeDetails);
         }
     }
